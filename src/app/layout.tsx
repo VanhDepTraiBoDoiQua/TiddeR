886a5591ef85bc2eeba3f9ec686cf737bdbce297
Caubechimto <mgmgd2004@gmail.com>
2025-03-29 15:15:43 +0700

    Add Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,34 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import {Inter} from "next/font/google";
+import {cn} from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
-  title: "TiddeR",
-  description: "Everything you need",
+    title: "TiddeR",
+    description: "Everything you need",
 }
 
+const inter = Inter({subsets: ["latin"]});
+
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang='en' className={cn(
+            "bg-white text-slate-900 antialiased light",
+            inter.className
+        )}>
+        <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+
+        {/*NAVBAR*/}
+        <Navbar/>
+
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+        </div>
+        </body>
+        </html>
+    )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import {Icons} from "@/components/Icons";
+import {buttonVariants} from "@/components/ui/Button";
+
+const Navbar = () => {
+    return (
+        <div className="fixed top-0 inset-x-0
+            h-fit bg-zinc-100 border-b
+            border-zinc-300 z-[10] py-2"
+        >
+            <div className="container max-w-7xl h-full
+                mx-auto flex items-center
+                justify-between gap-2"
+            >
+
+                {/*LOGO*/}
+                <Link href="/" className="flex gap-2 items-center">
+                    <Icons.logo className="h-10 w-10 sm:h-8
+                        sm:w-8"
+                    />
+                    <p className="hidden text-zinc-700 text-sm
+                        font-medium md:block"
+                    >
+                        TiddeR
+                    </p>
+                </Link>
+
+                {/*TODO: CREATE SEARCH BAR*/}
+                {/*SEARCH BAR*/}
+
+                <Link href="/sign-in" className={buttonVariants()}>
+                    Sign in
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
